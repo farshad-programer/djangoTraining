d524d12d1de7719ee3816f26580486e9c5083bfe
farshad bagheri <farshad65bagheri05@gmail.com>
2024-06-30 06:39:05 -0700

    generic without mixins

--- account/views.py
+++ account/views.py
@@ -1,6 +1,4 @@
-from django.http import Http404
-from rest_framework import generics, mixins, status
-from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Post
 from .serializers import postSerializer
@@ -8,34 +6,14 @@ from .serializers import postSerializer
 # Create your views here.
 
 
-class PostListView(
-    generics.GenericAPIView, mixins.CreateModelMixin, mixins.ListModelMixin
-):
+class PostListView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = postSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
 # CRUD --create , retrieve ,
 class PostDetailView(
-    generics.GenericAPIView,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    mixins.RetrieveModelMixin,
+    generics.RetrieveUpdateDestroyAPIView,
 ):
     queryset = Post.objects.all()
     serializer_class = postSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
