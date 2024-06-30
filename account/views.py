a804a7da8470bd0676ed4fdfe558898bd02e9331
farshad bagheri <farshad65bagheri05@gmail.com>
2024-06-30 06:33:22 -0700

    genericView sumerized code

--- account/views.py
+++ account/views.py
@@ -1,7 +1,6 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import generics, mixins, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Post
 from .serializers import postSerializer
@@ -9,49 +8,34 @@ from .serializers import postSerializer
 # Create your views here.
 
 
-class PostListView(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = postSerializer(instance=posts, many=True)  
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class PostListView(
+    generics.GenericAPIView, mixins.CreateModelMixin, mixins.ListModelMixin
+):
+    queryset = Post.objects.all()
+    serializer_class = postSerializer
 
-    def post(self, request):
-        serializer = postSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 # CRUD --create , retrieve ,
-class PostDetailView(APIView):
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
+class PostDetailView(
+    generics.GenericAPIView,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.RetrieveModelMixin,
+):
+    queryset = Post.objects.all()
+    serializer_class = postSerializer
 
-    def get(self, request, pk):
-        try:
-            posts = Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response(
-                {"detail": "there is no post"}, status=status.HTTP_404_NOT_FOUND
-            )
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-        # serializer = postSerializer(posts, many=True)
-        data = postSerializer(posts).data
-        return Response(data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = postSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
